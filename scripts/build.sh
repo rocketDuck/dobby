00388b13534c880795fd19ca151cdd7dd19996b7
Florian Apolloner <florian@apolloner.eu>
2020-10-08 22:16:56 +0200

    Properly fixed pex builds (constraints vs requirements)

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,12 @@ set -e
 mkdir -p bin dist repo
 rm -f bin/* dist/* repo/*
 poetry build
-poetry export --without-hashes --format=requirements.txt --output=requirements-pre.txt
-grep -Ev "(immutables|contextvars)==" requirements-pre.txt > requirements.txt
+poetry export --without-hashes --format=requirements.txt --output=requirements.txt
 pip wheel --wheel-dir=repo --constraint=requirements.txt PyYAML
 # Hack, but PyYAML in that configuration is actually a universal wheel
 mv repo/PyYAML-5.3.1-cp38-cp38-linux_x86_64.whl repo/PyYAML-5.3.1-py3-none-any.whl
 pex dist/dobby-*.whl --output-file=bin/dobby --script=dobby --disable-cache --no-compile --no-build --no-use-system-time \
-    --requirement=requirements.txt --python-shebang='/usr/bin/env python3' \
+    --constraints=requirements.txt --python-shebang='/usr/bin/env python3' \
     --find-links=file://`pwd`/repo \
     --python=python3.7 --python=python3.8 \
     --platform=manylinux1-x86_64-cp-37-m --platform=manylinux1-x86_64-cp-38-cp38 \
