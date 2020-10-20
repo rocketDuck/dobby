295111ffbcfe0bea55b03070bdc0fe676c978c37
Florian Apolloner <florian@apolloner.eu>
2020-10-20 15:39:28 +0200

    Fixed interpreter resolution of pex binaries.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ mv repo/PyYAML-5.3.1-cp38-cp38-linux_x86_64.whl repo/PyYAML-5.3.1-py3-none-any.w
 pex dist/dobby-*.whl --output-file=bin/dobby --script=dobby --disable-cache --no-compile --no-build --no-use-system-time \
     --constraints=requirements.txt --python-shebang='/usr/bin/env python3' \
     --find-links=file://`pwd`/repo \
-    --python=python3.7 --python=python3.8 \
+    --interpreter-constraint='CPython>=3.7,<3.9' \
     --platform=manylinux1-x86_64-cp-37-m --platform=manylinux1-x86_64-cp-38-cp38 \
     --platform=win-amd64-cp-37-m --platform=win-amd64-cp-38-cp38 \
     --platform=macosx-10.6-intel-cp-37-m --platform=macosx-10.9-x86_64-cp-38-cp38
